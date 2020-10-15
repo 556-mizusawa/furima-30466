c71aac6f820314585c3bfe592a5a58148c4548de
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-15 19:27:06 +0900

    新規登録テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   validates :nickname,              presence: true
   validates :birthday,              presence: true
   with_options presence: true do
-    #名前 全角かな、カナ、
+    #名前 全角漢字、かな、カナ
     validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
-    #フリガナ 全角カタカ
+    #フリガナ 全角カタカナ
     validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
     validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              { Faker::Name.initials(number: 6) }
+    nickname              { Faker::Name.initials(number: 1) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password}
+    password              { "tech556" }
+    password_confirmation { password }
     last_name             { "山田" }
     first_name            { "太郎"}
     last_name_reading     { "ヤマダ" }
