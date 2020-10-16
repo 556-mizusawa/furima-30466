af5f54fa8851663287ef0e94775387908f5321ea
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 15:12:12 +0900

    days_idをday_idに変更に伴う各ファイルの記述変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_id, :prefecture_id, :days_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
