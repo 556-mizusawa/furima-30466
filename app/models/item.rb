4df058f2991dd9bc86dc923126c0ff2862ecc41b
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 15:05:15 +0900

    ActiveHash_prefecture作成+その他ActiveHash:id修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, :status, :delivery_fee,
+  belongs_to_active_hash :genre, :status, :delivery_fee, :prefecture,
 
   belongs_to :user
   has_one_attached :image
@@ -9,6 +9,6 @@ class Item < ApplicationRecord
   validates :title, :catch_copy, :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, :price, presence: true
   
   #ActiveHash_idバリデーション
-  validates :genre_id, :status_id, :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :genre_id, :status_id, :delivery_fee_id, :prefecture_id,  numericality: { other_than: 0 } 
 end
 
