aa6b46b0879b64bdcf0ec804775764b554bbceaf
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 12:15:06 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
     
+  end
+
+  private
+  def prototype_params
+    params.require(:items).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_id, :prefecture_id, :days_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one_attaced :image
+  has_one_attached :image
 
   validates :title,          presence: true
   validates :catch_copy,     presence: true
