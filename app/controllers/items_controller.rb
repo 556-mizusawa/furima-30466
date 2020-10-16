5a556a68b305c987e99534c6f7b62684d4be502d
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 21:10:22 +0900

    コントローラー、モデル、view修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
   #通常バリデーション 
   validates :title, :catch_copy, :genre, :status, :delivery_fee, :prefecture, :day, :price, presence: true
-  
+
   #ActiveHash_idバリデーション
   validates :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, numericality: { other_than: 0 } 
 end
