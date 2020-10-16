d19f446bc9894947361b312e7a6a2a8fca48ee65
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 21:04:46 +0900

    手数料計算機能実装

---
+++ app/javascript/count.js
@@ -0,0 +1,15 @@
+function count() {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  itemPrice.addEventListener("keyup", ()  => {
+    const totalPrice = itemPrice.value;
+    totalPrice.innerHTML
+    addTaxPrice.innerHTML = Math.floor(totalPrice * 0.1);
+    profit.innerHTML = Math.floor(totalPrice * 0.9);
+  });
+}
+
+addEventListener("load", count);
+
