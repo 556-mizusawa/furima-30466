1566e9752cc6cb539ffc231339cbe4983435572d
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-16 11:43:03 +0900

    itemモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  
+  validates :title,          presence: true
+  validates :catch_copy,     presence: true
+  validates :genre_id,       presence: true
+  validates :status_id,      presence: true
+  validates :delivery_id,    presence: true
+  validates :prefecture_id,  presence: true
+  validates :days_id,        presence: true
+  validates :price,          presence: true
 end
