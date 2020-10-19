ee094407a1c04472fbc7679fd06ef1035a9371b5
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-19 12:06:28 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,30 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, :status, :delivery_fee, :prefecture, :day
+  belongs_to_active_hash :genre 
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
 
   belongs_to :user
   has_one_attached :image
 
   #通常バリデーション 
-  validates :title, :catch_copy, :genre, :status, :delivery_fee, :prefecture, :day, :price, presence: true
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :catch_copy 
+    validates :genre
+    validates :status
+    validates :delivery_fee
+    validates :prefecture
+    validates :day
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end
 
+  #金額範囲のバリデーション 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
   #ActiveHash_idバリデーション
-  validates :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, numericality: { other_than: 0 } 
+  validates :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, numericality: { other_than: 0, message: "Select"} 
 end
 
