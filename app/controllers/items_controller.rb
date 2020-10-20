d469a741ecfc7cb18b5dbea2d0634d1a1126a1a7
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-20 11:00:20 +0900

    show.html、showアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :catch_copy, :genre_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
