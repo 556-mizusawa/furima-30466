a4fc7c926eaabe2ff0f7afa52010c9f30cb315f7
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-22 19:31:22 +0900

    Orderコントローラーアクション、およびルーティング設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
   def index
-
+    @customer_order = CustomerOrder.new
   end
 
   def create
-    
+    @customer_order = CustomerOrder.new(order_params)
+    if @customer_order.valid?
+      @customer_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:customer_order).permit(:postal_code, :prefecture_id, :city, :house_number, :tell_number).merge(token: params[:token])
   end
 end
