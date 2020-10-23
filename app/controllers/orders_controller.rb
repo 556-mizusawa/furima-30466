c6dd85844626158f6dc9aefc5c3f84f6be5f99a2
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-23 11:44:32 +0900

    Formオブジェクトコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
     @customer_order = CustomerOrder.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -16,5 +17,9 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:customer_order).permit(:postal_code, :prefecture_id, :city, :house_number, :tell_number).merge(token: params[:token])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :price,).merge(user_id: current_user.id)
   end
 end
