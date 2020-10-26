a7bc21bcb73224a0df0b69f8f536f3560b31bc1e
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-10-26 15:45:38 +0900

    formオブジェクト、およびorderコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,22 +5,32 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @customer_order = CustomerOrder.new(order_params)
     if @customer_order.valid?
+      pay_item
       @customer_order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
   def order_params
-    params.require(:customer_order).permit(:postal_code, :prefecture_id, :city, :house_number, :tell_number).merge(token: params[:token])
+    params.require(:customer_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :tell_number, :token).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 
   def item_params
-    params.require(:item).permit(:title, :price,).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :price).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -12,7 +12,7 @@ class CustomerOrder
   end
 
   def save
-    order = Order.create(user_id: user.id, item_id: item.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, tell_number: tell_number, order_id: order.id)
   end
 end
